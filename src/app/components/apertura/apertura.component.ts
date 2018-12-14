27ca277c4436e032617088eacaa7f48af5a510e3
=Argenis Gutierrez <=argenis061274@hotmail.com>
2018-12-14 02:21:06 -0400

    Terminando el  modulo de apertura, faltan validaciones

--- src/app/components/apertura/apertura.component.ts
+++ src/app/components/apertura/apertura.component.ts
@@ -1,36 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient , HttpParams, HttpHeaders} from '@angular/common/http';
-import {Post} from '../post';
 import { Observable } from 'rxjs';
-
 
 @Component({
   selector: 'app-apertura',
   templateUrl: './apertura.component.html',
   styleUrls: ['./apertura.component.css']
 })
+
 export class AperturaComponent implements OnInit {
+  /* Declaraciones de variables */
   newPost: Observable<any>;
-  rutaInsertarCliente= 'https://app.alegra.com/api/v1/contacts';
-  readonly ROOT_URL = this.rutaInsertarCliente;
-  
+  readonly ROOT_URL = 'https://app.alegra.com/api/v1/items/?start=0';
+  posts: Observable<any>;
+  cantCelExi:number = 0;
+  cantCelAdd:number = 0;
+  x:number;
+  nombreCliente : string;
 
-  constructor(private http:HttpClient) { }
-  
-  aperturaCeldas(){
-      /*Registrar solo las 20 celdas*/
-      
-      const dataAddCliente ={
-        name: "Consultoría diseño de billeteras",
-        price : 30
+  constructor(private http:HttpClient) { 
+    /*Buscar la cantidad de celdas disponibles*/
+    let headers = new HttpHeaders().set('authorization','Basic YXJnZW5pczA2MTI3NEBob3RtYWlsLmNvbTo2NjdkMmI4Y2UzMDFkOWYyZmJiMQ==');
+    this.posts = this.http.get(this.ROOT_URL + '/posts', {headers})
+    .subscribe(
+      (data:any[]) => {
+        if(data.length > 0 && data.length <= 20){
+          //calcular diferencia de los puestos a crear
+          this.cantCelAdd = 20 - data.length;        
+          this.cantCelExi = data.length;
+        }
       }
-        
-      let headers = new HttpHeaders().set('authorization','Basic YXJnZW5pczA2MTI3NEBob3RtYWlsLmNvbTo2NjdkMmI4Y2UzMDFkOWYyZmJiMQ==');
-      this.newPost = this.http.post(this.ROOT_URL, dataAddCliente, {headers : headers})
-      
+    )
+
+   }
+  
+   setCeldas(){
+      /*Registrar solo las 20 celdas*/
+      for (this.x=1; this.x<=this.cantCelAdd; this.x++) { 
+        //sentencias a ejecutar en cada iteración 
+        this.cantCelExi++
+        const dataAddCliente = {name: "Estacionamiento Celda: ("+this.cantCelExi+")" , price : 30}
+        let headers = new HttpHeaders().set('authorization','Basic YXJnZW5pczA2MTI3NEBob3RtYWlsLmNvbTo2NjdkMmI4Y2UzMDFkOWYyZmJiMQ==');
+        this.newPost = this.http.post("https://app.alegra.com/api/v1/items", dataAddCliente, {headers : headers})
+        .subscribe((data:any) =>{}
+        )
+      }
   }  
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {  }
 }
